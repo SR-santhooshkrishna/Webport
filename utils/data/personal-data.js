478bc7ee542e445bd239d6c11a2ac9ddb043084d
Santhoo-krish <rameshsanthoo@gmail.com>
2025-01-13 13:16:33 +0530

    Done new changes and added what ever needed

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "SANTHOOSHKRISHNA RAMESH",
   profile: '/profile.JPEG',
   designation: "Software Developer",
-  description: "My SANTHOOSHKRISHNA RAMESH. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "I am Santhooshkrishna Ramesh, a passionate and dedicated programmer with a strong enthusiasm for exploring and mastering diverse technologies. I thrive on solving complex problems, continuously learning, and adapting to the dynamic world of technology. While my core expertise lies in JavaScript, I enjoy applying my skills across various domains, from web development to backend systems, API integrations, and beyond. My ability to quickly grasp new concepts and technologies allows me to contribute effectively across a wide range of projects.I am committed to building solutions that are efficient, innovative, and impactful, while also ensuring they are accessible and user-friendly. I am open to opportunities that challenge my skills and allow me to make meaningful contributions in technology-driven environments.",
   email: 'rameshsanthoo@gmail.com',
   phone: '+918825579900',
   address: '169, West Ramp part Thanjavur - Pin(613009) ',
