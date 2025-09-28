3937375b1bf2298b2dd751271ac1860200d5c64f
Santhoo-krish <87886417+SR-santhooshkrishna@users.noreply.github.com>
2025-09-28 20:20:46 +0530

    Update personal-data.js
    changes

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,11 +1,11 @@
 export const personalData = {
   name: "SANTHOOSHKRISHNA RAMESH",
   profile: '/profile.JPEG',
-  designation: "Software Developer",
-  description: "I’m someone who believes in figuring things out, staying curious, and delivering real impact. I may not know everything on Day 1 — but I learn fast, adapt quickly, and take ownership to get things done. With a solid foundation in software engineering and hands-on experience delivering real-world projects, I’ve built tools that improve workflows, solve problems, and support business goals. Whether it’s building secure APIs, integrating systems, or debugging under pressure — I show up, dive deep, and follow through. Whether it’s in tech, healthcare, or life in general — I bring a mindset to learn, adapt quickly, and contribute meaningfully. I’ve collaborated across teams and timelines, always focusing on clarity, consistency, and results. I value quality over shortcuts — and learning over pretending. 📍Open to global opportunities — excited to contribute where I can build, grow, and make a difference. I love creating beautifully simple things — and I love what I do. Learn new things more & every day....♾️"
+  designation: "Software Engineer",
+  description: "I’m someone who believes in figuring things out, staying curious, and delivering real impact. I may not know everything on Day 1 — but I learn fast, adapt quickly, and take ownership to get things done. With a solid foundation in software engineering and hands-on experience delivering real-world projects, I’ve built tools that improve workflows, solve problems, and support business goals. Whether it’s building secure APIs, integrating systems, or debugging under pressure — I show up, dive deep, and follow through. Whether it’s in tech, healthcare, or life in general — I bring a mindset to learn, adapt quickly, and contribute meaningfully. I’ve collaborated across teams and timelines, always focusing on clarity, consistency, and results. I value quality over shortcuts — and learning over pretending. 📍Open to global opportunities — excited to contribute where I can build, grow, and make a difference. I love creating beautifully simple things — and I love what I do. Learn new things more & every day....♾️",
   email: 'rameshsanthoo@gmail.com',
   phone: '+918825579900',
-  address: '169, West Ramp part Thanjavur - Pin(613009) ',
+  address: '📍 Thanjavur - Pin(613009) ',
   github: 'https://github.com/SR-santhooshkrishna',
   facebook: 'https://www.facebook.com',
   linkedIn: 'https://www.linkedin.com/in/sr-santhooshkrishna',
